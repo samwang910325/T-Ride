52ca95b2a5d1ea92c86d26a20a02756dca5cfac8
jasonhu <jasonhu1012@hotmail.com.tw>
2023-12-14 16:10:55 +0800

    fix

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,4 @@
 import configparser
-import os
 
 
 class Config:
@@ -7,5 +6,4 @@ class Config:
     config.read('config.ini')
 
     def get(key):
-        env_config = os.environ.get(key)
-        return Config.config[key] if env_config is None else env_config
+        return Config.config[key]
